a3e5a5a5ccf7c4b2ce6d4be7d75b43c1d3db82a3
not1214 <91748221+not1214@users.noreply.github.com>
2022-03-06 23:28:48 +0900

    PHP practice 連想配列

---
+++ index3.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <title>Practice</title>
+  <link rel="stylesheet" type="text/css" href="stylesheet.css">
+</head>
+
+<body>
+
+  <?php
+  $name = 'たろう';
+  // 'こんにちは！'という文字列と$nameを連結してechoしてください
+  // echo 'こんにちは！'.$name;
+  echo "こんにちは！{$name}さん";
+
+  ?>
+
+</body>
+
+</html>
